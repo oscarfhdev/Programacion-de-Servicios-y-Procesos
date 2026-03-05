5a4ef4bb6d55dd4884eac7d5016dc1a09b532d01
oscarfhdev <oscarfh.dev@gmail.com>
2026-03-05 10:02:28 +0100

    initial project

---
+++ AF5 (RA4 - RA5)/Gestion_Empleados/src/main/java/psp/af5/Gestion_Empleados/GestionDeEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package psp.af5.Gestion_Empleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEmpleadosApplication.class, args);
+	}
+
+}
---
+++ AF5 (RA4 - RA5)/Gestion_Empleados/src/test/java/psp/af5/Gestion_Empleados/GestionDeEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package psp.af5.Gestion_Empleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
