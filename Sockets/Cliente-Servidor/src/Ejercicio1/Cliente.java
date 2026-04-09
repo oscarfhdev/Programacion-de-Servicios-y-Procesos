881fd76c1feb54e6be178830d8bf1872afda2ade
oscarfhdev <oscarfh.dev@gmail.com>
2026-04-09 17:13:48 +0200

    cambios en el ejercicio

--- Sockets/Cliente-Servidor/src/Ejercicio1/Cliente.java
+++ Sockets/Cliente-Servidor/src/Ejercicio1/Cliente.java
@@ -1,67 +1,54 @@
 package Ejercicio1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
 
 public class Cliente {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+                System.out.println("Cliente conectado");
 
-        System.out.println("--- Iniciar conexion con el servidor ---");
-        System.out.println("--- 1 : SI ");
-        System.out.println("--- 2 : NO ");
-        System.out.print("--- : ");
-        int op = sc.nextInt();
-        switch (op){
-            case 1:
-                try(Socket socket = new Socket("localhost",1234);
-                    PrintWriter out = new PrintWriter(
-                            socket.getOutputStream(),true
-                    );
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream()));
-                ){
+        try (
+                Socket socket = new Socket("localhost", 1234);
+                PrintWriter out = new PrintWriter(
+                socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
 
-                    System.out.println("Conexion correcta ... ");
+                BufferedReader teclado = new BufferedReader(
+                new InputStreamReader(System.in));
+            ) {
 
-                    String opMenu = "";
-                    while(!opMenu.equals("Q")){
-                        System.out.println("--- CIFRAR : Cifrado ");
-                        System.out.println("--- DESCIFRAR : Descifrado ");
-                        System.out.println("--- Q : Terminar Conexion ");
-                        System.out.print("Inserta la opcion --- : ");
-                        sc.nextLine();
-                        out.println(sc.nextLine());
 
-                        if(in.readLine().equals("y")){
-                            System.out.print("Inserta la cadena --- : ");
-                            opMenu = sc.nextLine();
-                            out.println(opMenu);
-                        } else {
-                            socket.close();
-                            break;
-                        }
-
-                        String cadenaTrabajada = in.readLine();
-                        if(cadenaTrabajada != null){
-                            System.out.println("Cadena: " + cadenaTrabajada);
-                        }
-                    }
-
-                    if(opMenu.equals("Q")){
-                        out.println("Q");
-                    }
-
-                }catch (Exception e){
-
-                }
-                break;
-            case 2:
-                System.out.println("Saliendo del programa ...");
+                                        
+        out.println(teclado.readLine());
+        String respuesta=in.readLine();
+        if (respuesta.equals("Pasame la cadena")){
+            out.println(teclado.readLine());
+            System.out.println("cifrado: "+in.readLine());
+        }else{
+            System.out.println(respuesta); 
         }
+
+        // mandarle a Descifrar ABC
+        out.println(teclado.readLine());
+        respuesta=in.readLine();
+        if (respuesta.equals("Pasame la cadena")){
+            out.println(teclado.readLine());
+            System.out.println("descifrado: "+in.readLine());
+        }else{
+            System.out.println(respuesta); 
+        }
+
+        out.println(teclado.readLine());
+
+        System.out.println(in.readLine());
+
+
+        
+    }catch (IOException e){
+        System.err.println("Error en conexión");
+    }catch (ArithmeticException a){
+            System.err.println("División sobre 0"+a.getMessage());
     }
+}
 }
--- Sockets/Cliente-Servidor/src/Ejercicio1/Servidor.java
+++ Sockets/Cliente-Servidor/src/Ejercicio1/Servidor.java
@@ -1,91 +1,82 @@
 package Ejercicio1;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Servidor {
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(1234);
-             Socket socket = server.accept();
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+        System.out.println("Servidor arrancado y esperando...");
+        try (
+            ServerSocket server = new ServerSocket(1234);
+            Socket cliente = server.accept();
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(cliente.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                cliente.getOutputStream(), true);
+        ){
+            String operacion;
+            while((operacion=in.readLine())!=null && !operacion.equals("EXIT")){
+                String cadena=null;
 
-            String op = in.readLine();
-            System.out.println(op);
+                switch (operacion){
+                    case "CIFRAR":
+                    case "DESCIFRAR":
+                        out.println("Pasame la cadena");
+                        cadena=in.readLine();
+                    break;
+                    default:
+                        out.println("Operación no permitida");
+                }
 
-            if (op != null && !op.equals("") && !op.equals("Q")) {
-                out.println("y");
-            } else if (op.equals("Q")) {
-                socket.close();
+                out.println(cifrar_descifrar(cadena,operacion,3));
             }
 
-            String cadena = in.readLine();
-            switch (op) {
-                case "CIFRAR":
-                    cadena = cifrar(cadena);
-                    break;
-                case "DESCIFRAR":
-                    cadena = desCrifrar(cadena);
-                    break;
-                default:
-                    break;
-            }
+            out.println("ADIOS");
+            
 
-            if (cadena != null) {
-                out.println(cadena);
-            }
-
-        } catch (Exception e) {
-
+        }catch (IOException e){
+            System.err.println("Error de conexión");
+        }catch (ArithmeticException a){
+            System.err.println("División sobre 0"+a.getMessage());
         }
+        
+    
     }
 
-    public static String cifrar(String cadena) {
-        String cadenaCifrada = "";
-        int rangoA_Z = ('z' - 'a') + 1;
+    public static String cifrar_descifrar(String cadena,String operacion,int desplazamiento){
+        int rango=('z'-'a')+1;
 
-        for (char c : cadena.toCharArray()) {
+        String resultado="";
+        char c;
+        switch (operacion){
+            case "CIFRAR":
+                
+                for (int i=0;i<cadena.length();i++){
+                    c=cadena.charAt(i);
 
-            if (c >= 'a' && c <= 'z') {
+                    if (c>='a' && c<='z'){
+                        resultado+= (char) (((c-'a')+desplazamiento)%rango + 'a');
+                    }else if (c>='A' && c<='Z'){
+                        resultado+= (char) (((c-'A')+desplazamiento)%rango + 'A');
+                    }else{
+                        resultado+=c;
+                    }
+                }
+            break;
+            case "DESCIFRAR":
+                for (int i=0;i<cadena.length();i++){
+                    c=cadena.charAt(i);
 
-                int pos = (((c - 'a') + 3) + rangoA_Z) % rangoA_Z;
-                cadenaCifrada += (char) ('a' + pos);
-
-            } else if (c >= 'A' && c <= 'Z') {
-
-                int pos = (((c - 'A') + 3) + rangoA_Z) % rangoA_Z;
-                cadenaCifrada += (char) ('A' + pos);
-
-            }
-
+                    if (c>='a' && c<='z'){
+                        resultado+= (char) (((c-'a')-desplazamiento+rango)%rango + 'a');
+                    }else if (c>='A' && c<='Z'){
+                        resultado+= (char) (((c-'A')-desplazamiento+rango)%rango + 'A');
+                    }else{
+                        resultado+=c;
+                    }
+                }
+            break;
         }
-
-        return cadenaCifrada;
-    }
-
-    public static String desCrifrar(String cadena) {
-        String cadenaDescifrada = "";
-        int rangoA_Z = ('z' - 'a') + 1;
-
-        for (char c : cadena.toCharArray()) {
-
-            if (c >= 'a' && c <= 'z') {
-
-                int pos = (((c - 'a') - 3) + rangoA_Z) % rangoA_Z;
-                cadenaDescifrada += (char) ('a' + pos);
-
-            } else if (c >= 'A' && c <= 'Z') {
-
-                int pos = (((c - 'A') - 3) + rangoA_Z) % rangoA_Z;
-                cadenaDescifrada += (char) ('A' + pos);
-
-            }
-
-        }
-
-        return cadenaDescifrada;
+        return resultado;
     }
 }
